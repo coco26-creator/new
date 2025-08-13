8225a2c91aabe6c14a8391ce758f1ca4b4b10190
coco26-creator <Cocolenis2@gmail.com>
2025-08-13 16:21:54 +0800

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import ProductsSection from '../components/ProductsSection';
+import ContactSection from '../components/ContactSection';
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <AboutSection />
+      <ProductsSection />
+      <ContactSection />
+    </div>
+  );
+}
